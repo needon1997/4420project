69160b5452a23e3e1c1ca5a1c48b15282933d715
eden <weiy3458@gmail.com>
2021-03-14 22:05:20 -0500

    fix dependency

--- suffixTree/suffix_tree_test.go
+++ suffixTree/suffix_tree_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"testing"
 )
-import suffix "text-indexing"
+import suffix "4420project/suffixTree"
 
 func TestBuild(t *testing.T) {
 	tree := suffix.NewSuffixTree("abcabxabcd")
@@ -18,14 +18,4 @@ func BenchmarkBuild(b *testing.B) {
 		tree := suffix.NewSuffixTree("yH823YG7f3xVmAXqE40lc1346reXDMfc0WE6cHmnGUkd6wWG2tq9Rgi9mXcQx83DRv6Zi7VOCM6v6ws2sXhDZo0wog3tiAzo9h79ZqJA2V8uvkYK1wp7RvHr1Q6EBNHAa2OKV3fGz8V0Dz3EucA9qwa7unLfPhG6vMuS5M797jpyOhp7fhi8e1DEqGiPnDm7k6M2O61zvlRdQEoBw30zEGK2y2fHXfvg28a6gYXPjvz4Vw76YgQMqw0dz7kdy17i88E6TIFob5JUBn06qWm6038ZJ7s7u70xLMsoE8HmkceH8y277qC2iQRSXXitoikF393668FXZI3Z1qC7sy7n9795QJ39vqT797Y91yLI7yfn92i0pMhQjP01qCQV3Py2FfQ3o06qQm36WzxXFAsxtbd25fF0dp98WO55wu7S1MAfs26qbojZHxt6AGyQedUOVWnF6SHwI9qLdHigkUdU2NQFoA3849gRwsgHTrFlnl3ol842VErx7qetjEa77mTB8843z65vhH94ivmT2Z8thknAIalFt46ma5ZGKNK5tX90Hys725SPS75")
 		tree.ToSuffixArray()
 	}
-}
-
-func TestRadix(t *testing.T) {
-	//arr := []string{"abc","cde","aaa","bbb","123","jkl"}
-	//barr := suffix.ToByte(arr)
-	//suffix.Radixsort(&barr,3)
-	t3 := suffix.Build("helloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworld")
-	t3_b := suffix.ToByte(t3)
-	suffix.Radixsort(&t3_b, 3)
-	fmt.Println(t3)
 }
